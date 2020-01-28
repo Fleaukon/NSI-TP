f936d7310ecd5dee4039c5421e46c5d11a2c9467
Professeur Gibaud <mail@professeurgibaud.ovh>
2020-01-28 18:19:26 +0100

    Exercice Compétence 4 ajoutés

--- TP Competence4 Concevoir.py
+++ TP Competence4 Concevoir.py
@@ -1,54 +1,37 @@
 """
 
-Exercice de Fondamentaux en NSI
-
-
-Dans ce fichier python, on va travailler les 4 grandes compétences en programmation.
-
-
-"""
-
-
-
-
-
-"""
-
-Compétence 1 : Lire
-
-"""
-
-
-
-
-
-
-
-
-
-"""
-
-Compétence 2 : Expliquer
-
-"""
-
-
-
-
-
-"""
-
-Compétence 3 : Comprendre
-
-"""
-
-
-
-
-
-
-"""
-
 Compétence 4 : Concevoir
 
 """
+
+
+
+"""
+
+Prérequis : Même si ici la feuille de papier et le stylo n'est pas obligatoire. Il est très fortement recommandé d'avoir un brouillon
+
+"""
+
+
+"""
+Exercice 1 : Faire des scripts, algorithmes réalisant les objectifs suivants
+"""
+
+
+# Objectif 1 : Calculer votre age en 2060
+
+# Objectif 2 : Pour un nombre donné calculer le carré du triple du nombre
+
+# Objectif 3 :On lance deux dés à six faces parfaitement équilibrés et on additionne les deux résultats obtenus. Écrire un programme qui modélise les lancers de ces deuxdés et qui donne le nombre n de lancers qu’il a fallu pour obtenir la somme 12. On veut voir le résultat des 2 dés à chaque tour.
+
+# Objectif 4 : Écrire un programme qui demande un entier positif ou nul et qui donne ensuite son nombre de chiffre.
+
+# Objectif 5 : Un joueur doit trouver un nombre mystère choisi de manière aléatoire par l’ordinateur. À chaque tour, le joueur propose un nombre et l’ordinateur indique si le nombre mystère est plus petit ou plus grand que le nombre proposé jusqu’à ce que le joueur trouve le nombre mystère.Écrire un programme permettant de jouer à ce jeu avec un nombre à chercher compris entre zéro et 100.  Compléter ce programme afin de faire afficher le nombre d’essais effectués.
+
+# Objectif 6 : Faites une fonction qui prends en entrée 3 entiers n,a,b et qui donne en sortie une liste de n nombres aléatoires compris entre a et b.
+
+# Objectif 7 : Faire une fonction qui prend en entrée une liste et qui donne en sortie un dictionnaire avec comme clé les items de la liste et comme valeur le nombre de fois où cette clé est dans la liste. Exemple ci dessous
+
+L = [1,1,1,1,3,3,5,"b","c"]
+dico = {1:4,3:2,"b":1,"c":1}
+
