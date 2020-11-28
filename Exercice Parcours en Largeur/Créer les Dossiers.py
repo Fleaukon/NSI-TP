6443601d9c26da5913a10814fe4f5b44d01179c8
GibaudNewton <gibaudsylvain@gmail.com>
2020-11-28 17:30:54 +0100

    Consigne mise à jour

--- Exercice Parcours en Largeur/Créer les Dossiers.py
+++ Exercice Parcours en Largeur/Créer les Dossiers.py
@@ -1,15 +1,16 @@
 import random
 import os
 import numpy as np
+os.chdir(r"C:\Users\Utilisateur\Desktop\Python Temp\NSI-TP\Exercice Parcours en Largeur")
 root = os.getcwd() #Naviguer avant de lancer le programme sinon ça va envahir votre dossier utilisateur
 
 File = []
 Compteur = 0
 Alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+Liste = []
 
-
-def MkDos(p,path): #path chemin dossier, p proba de créer un dossier
-    global File,Compteur
+def MkDos(p,v,path): #path chemin dossier, p proba de créer un dossier
+    global File,Compteur,Liste
     os.chdir(path)
     u = random.random()
     v = random.random()
@@ -18,6 +19,10 @@ def MkDos(p,path): #path chemin dossier, p proba de créer un dossier
         os.mkdir(str(Compteur))
         os.chdir(str(Compteur))
         # Ajouter ouvrir document avec proba v et écrire Alphabet[compteur%26]
+        if random.random()<p:
+            with open("text.txt","w+") as file:
+                file.write(Alphabet[Compteur%26])
+                Liste.append(Alphabet[Compteur%26])
         temp_path = os.getcwd()
         File.append(temp_path)
         os.chdir("..")
@@ -26,15 +31,18 @@ def MkDos(p,path): #path chemin dossier, p proba de créer un dossier
     os.chdir(root)
 
 
-def Boucle(p,t): #p proba initiale, t taux de décroissance
-    global File,Compteur
-    MkDos(p,root)
+def Boucle(v,p,t): #p proba initiale, t taux de décroissance
+    global File,Compteur,Liste
+    MkDos(v,p,root)
     while len(File)>0:
         pathDos = File.pop(0)
         p = max(0, p*np.exp(-t*Compteur))
         print(len(File))
-        MkDos(p,pathDos)
+        MkDos(p,v,pathDos)
 
 
-#Enlever le commentaire de la ligne suivante et lancer la Fonction Boucle
-#Boucle(0.9,0.000001)
+#Enlever le commentaire de la ligne suivante et lancer la Fonction Boucle.
+
+#Boucle(0.5,0.9,0.000001)
+
+
